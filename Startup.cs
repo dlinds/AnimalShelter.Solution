fb4feb903d0bc3ec3cf14818c892b70c9bd2871c
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-04-01 10:33:49 -0700

    add swashbuckle to swagger

--- Startup.cs
+++ Startup.cs
@@ -10,9 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using System.Reflection;
+using System.IO;
 
 namespace AnimalShelter.Solution
 {
@@ -30,11 +32,24 @@ namespace AnimalShelter.Solution
     {
 
       services.AddDbContext<AnimalShelterContext>(opt =>
-                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+          opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter.Solution", Version = "v1" });
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Animal Shelter API",
+          Version = "v1",
+          Description = "n ASP.NET Core Web API for managing an Animal Shelter",
+          Contact = new OpenApiContact
+          {
+            Name = "Daniel Lindsey",
+            Url = new Uri("https://www.github.com/dlinds")
+          }
+        });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
       });
     }
 
@@ -44,8 +59,20 @@ namespace AnimalShelter.Solution
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter.Solution v1"));
+        app.UseSwagger(c =>
+        {
+          c.SerializeAsV2 = true;
+        });
+        app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Animal Shelter API V1");
+        });
+        // app.UseEndpoints(endpoints =>
+        // {
+        //   endpoints.MapControllers();
+        // });
+
+        // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter.Solution v1"));
       }
 
       // app.UseHttpsRedirection();
